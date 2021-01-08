2c4d8928da64007df26becaae1262517f9d4072c
fedyok8 <fedyok8@yandex.ru>
2021-01-08 21:21:11 +0300

    create exit button

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/home/fedyok8/anaconda3/envs/pyqt/bin/python
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
-from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
 class Example(QWidget):
     def __init__(self):
@@ -20,6 +21,13 @@ class Example(QWidget):
         btn = QPushButton('Button1', self)
         btn.resize(btn.sizeHint())
         btn.move(10, 10)
+
+        button_exit = QPushButton('Quit', self)
+        button_exit.clicked.connect(
+            QCoreApplication.instance().quit
+        )
+        button_exit.resize(button_exit.sizeHint())
+        button_exit.move(100, 100)
 
         self.show()
 
