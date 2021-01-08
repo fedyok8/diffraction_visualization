246b5ad23db75e1eb517e902fff8f3a5cd5323f2
fedyok8 <fedyok8@yandex.ru>
2021-01-08 20:31:03 +0300

    create empty window with PyQt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/home/fedyok8/anaconda3/envs/pyqt/bin/python
 
-
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
@@ -9,9 +8,9 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
+    w.resize(500, 500)
+    w.move(0, 0)
+    w.setWindowTitle('diffraction')
     w.show()
 
     sys.exit(app.exec_())
