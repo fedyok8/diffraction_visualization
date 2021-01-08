1b6c22e2478708bef7fe9f4ac60c2f6158ad381f
fedyok8 <fedyok8@yandex.ru>
2021-01-08 20:49:43 +0300

    create OOP-style and add app icon

--- main.py
+++ main.py
@@ -2,15 +2,27 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.setGeometry(0, 0, 500, 500)
+        self.setWindowTitle('diffraction')
+        self.setWindowIcon(QIcon('./app_icon.jpeg'))
+
+        self.show()
+
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(500, 500)
-    w.move(0, 0)
-    w.setWindowTitle('diffraction')
-    w.show()
+    ex = Example()
 
     sys.exit(app.exec_())
