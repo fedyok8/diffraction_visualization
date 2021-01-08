d6825f6d1756ffd42d3007b5d342c444eb98b00a
fedyok8 <fedyok8@yandex.ru>
2021-01-08 21:09:23 +0300

    add empty button

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/home/fedyok8/anaconda3/envs/pyqt/bin/python
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtGui import QIcon, QFont
 
 class Example(QWidget):
     def __init__(self):
@@ -16,6 +16,10 @@ class Example(QWidget):
         self.setGeometry(0, 0, 500, 500)
         self.setWindowTitle('diffraction')
         self.setWindowIcon(QIcon('./app_icon.jpeg'))
+
+        btn = QPushButton('Button1', self)
+        btn.resize(btn.sizeHint())
+        btn.move(10, 10)
 
         self.show()
 
