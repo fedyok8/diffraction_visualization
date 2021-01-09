12b2d3900bcc5ff723d78ed3bfa56e6df7453117
fedyok8 <fedyok8@yandex.ru>
2021-01-10 00:58:27 +0300

    add graph to window

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 #!/home/fedyok8/anaconda3/envs/pyqt/bin/python
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget
+
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QPushButton,
+    QMessageBox,
+    QDesktopWidget,
+    QMainWindow,
+    QMenu,
+    QVBoxLayout,
+    QSizePolicy
+)
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+import random
 
 class Example(QWidget):
     def __init__(self):
@@ -11,27 +28,29 @@ class Example(QWidget):
 
         self.initUI()
 
-
     def initUI(self):
-        self.resize(500, 500)
+        self.resize(1000, 800)
         self.move_to_center()
 
         #if you want to define your own window geometry:
         # self.setGeometry(0, 0, 500, 500)
 
-        self.setWindowTitle('diffraction')
+        self.setWindowTitle('Example window')
         self.setWindowIcon(QIcon('./app_icon.jpeg'))
 
         btn = QPushButton('Button1', self)
         btn.resize(btn.sizeHint())
-        btn.move(10, 10)
+        btn.move(900, 10)
 
         button_exit = QPushButton('Quit', self)
         button_exit.clicked.connect(
             QCoreApplication.instance().quit
         )
         button_exit.resize(button_exit.sizeHint())
-        button_exit.move(100, 100)
+        button_exit.move(900, 100)
+
+        m = PlotCanvas(self, width=5, height=4)
+        m.move(0,0)
 
         self.show()
 
@@ -53,6 +72,29 @@ class Example(QWidget):
         center_point = QDesktopWidget().availableGeometry().center()
         window_rect.moveCenter(center_point)
         self.move(window_rect.topLeft())
+
+class PlotCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plot()
+
+    def plot(self):
+        data = [random.random() for i in range(25)]
+        ax = self.figure.add_subplot(111)
+        ax.plot(data, 'r-')
+        ax.set_title('PyQt Matplotlib Example')
+        self.draw()
+
 
 if __name__ == '__main__':
 
